a198f9955aa32a0aace083aacd0ff8e8ff93e699
Leonardo Carey <careycoder@gmail.com>
2021-11-01 18:59:12 -0300

    Version updated

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
     
-    const presets = [ '@babel/preset-env', '@babel/preset-react' ];
+    const presets = [ '@babel/preset-env', '@babel/preset-react', '@babel/react' ];
     const plugins = [ 'macros', 'react' ];
 
     return {
